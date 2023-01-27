ee8a1cbe2be05915bd4a526e8a14dada62d177a1
Mark J Koch <maehem@users.noreply.github.com>
2023-01-27 09:13:43 -0800

    Unused inport

--- src/main/java/com/maehem/flatlinejack/engine/matrix/MatrixNode.java
+++ src/main/java/com/maehem/flatlinejack/engine/matrix/MatrixNode.java
@@ -24,7 +24,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 
 /**
