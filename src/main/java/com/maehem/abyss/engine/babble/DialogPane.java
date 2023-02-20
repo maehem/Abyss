6eaaaf4a169c1815f3dc5aa0881679fa6e24aafe
Mark J Koch <maehem@users.noreply.github.com>
2023-02-20 10:10:06 -0800

    Add doCloseDialog() method. Ran reformat on code.

--- src/main/java/com/maehem/abyss/engine/babble/DialogPane.java
+++ src/main/java/com/maehem/abyss/engine/babble/DialogPane.java
@@ -13,7 +13,7 @@
     WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
     License for the specific language governing permissions and limitations 
     under the License.
-*/
+ */
 package com.maehem.abyss.engine.babble;
 
 import com.maehem.abyss.engine.Character;
@@ -30,7 +30,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -53,16 +52,15 @@ public class DialogPane extends BorderPane {
 
     public final static double FONT_SIZE = 30;
     public final static Color DROP_COLOR = new Color(0.0, 0.0, 0.0, 0.7);
-    
+
     private final ArrayList<DialogSheet2> dialogList = new ArrayList<>();
     private DialogSheet2 currentDialogSheet;
-    private VignetteTrigger port=null;
+    private VignetteTrigger port = null;
 
     private boolean actionDone;
     private final Character npc;
     private Player player;
-    
-    
+
     //private final FlowPane dialogPane = new FlowPane();
 //    private double width=0;
 //    private double height=0;
@@ -70,36 +68,34 @@ public class DialogPane extends BorderPane {
 //    private double boxY;
 //    private double boxW;
 //    private double boxH;
-    
     private final Text nameText = new Text("Character Name");
     private final Text dialogText = new Text(
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony. " +
-            "Dialog Text. Hello. I am a pretty pony."
+            "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony. "
+            + "Dialog Text. Hello. I am a pretty pony."
     );
     private final VBox answerButtonsBox = new VBox();
-    
-    public DialogPane( Character npc) {
+
+    public DialogPane(Character npc) {
         this.npc = npc;
-        setPrefSize(ViewPane.WIDTH*0.7, ViewPane.HEIGHT*0.8);
-        setMinSize(ViewPane.WIDTH*0.7, ViewPane.HEIGHT*0.8);
-        setLayoutX(ViewPane.WIDTH*0.15);
-        setLayoutY(ViewPane.HEIGHT*0.1);
+        setPrefSize(ViewPane.WIDTH * 0.7, ViewPane.HEIGHT * 0.8);
+        setMinSize(ViewPane.WIDTH * 0.7, ViewPane.HEIGHT * 0.8);
+        setLayoutX(ViewPane.WIDTH * 0.15);
+        setLayoutY(ViewPane.HEIGHT * 0.1);
         //setBackground(new Background(new BackgroundFill(Color.GRAY, new CornerRadii(20), Insets.EMPTY)));
         setEffect(new DropShadow(40, DROP_COLOR));
-        
-        
+
         HBox nameBox = new HBox(nameText);
-        nameBox.setPadding(new Insets(FONT_SIZE/2));
-        nameText.setFont(Font.font(FONT_SIZE*1.3));
+        nameBox.setPadding(new Insets(FONT_SIZE / 2));
+        nameText.setFont(Font.font(FONT_SIZE * 1.3));
         nameText.setText(npc.getName());
-        
+
         dialogText.setFont(Font.font(FONT_SIZE));
 
         TextFlow dialogTextFlow = new TextFlow(dialogText);
@@ -109,27 +105,27 @@ public class DialogPane extends BorderPane {
         VBox leftArea = new VBox(nameBox, dialogTextFlow);
         leftArea.setFillWidth(true);
         leftArea.setBackground(new Background(new BackgroundFill(
-                Color.GRAY, 
-                new CornerRadii(20,0,0,20, false), 
+                Color.GRAY,
+                new CornerRadii(20, 0, 0, 20, false),
                 Insets.EMPTY
         )));
-        
+
         // Clip the buttons area so that the drop shadow only shades the left edge.
-        Rectangle r = new Rectangle(ViewPane.WIDTH*0.7+40, ViewPane.HEIGHT*0.8);
+        Rectangle r = new Rectangle(ViewPane.WIDTH * 0.7 + 40, ViewPane.HEIGHT * 0.8);
         r.setArcHeight(20);
         r.setArcWidth(20);
         r.setLayoutX(-40);
         answerButtonsBox.setClip(r);
-        
-        answerButtonsBox.setPrefWidth(getPrefWidth()*0.5);
-        answerButtonsBox.setMinWidth(getPrefWidth()*0.5);
+
+        answerButtonsBox.setPrefWidth(getPrefWidth() * 0.5);
+        answerButtonsBox.setMinWidth(getPrefWidth() * 0.5);
         answerButtonsBox.setBackground(new Background(new BackgroundFill(
-                Color.DARKGRAY, 
-                new CornerRadii(0, 20, 20, 0, false), 
+                Color.DARKGRAY,
+                new CornerRadii(0, 20, 20, 0, false),
                 Insets.EMPTY)
         ));
-        answerButtonsBox.setPadding(new Insets(FONT_SIZE/2.0));
-        answerButtonsBox.setSpacing(FONT_SIZE/2.0);
+        answerButtonsBox.setPadding(new Insets(FONT_SIZE / 2.0));
+        answerButtonsBox.setSpacing(FONT_SIZE / 2.0);
         answerButtonsBox.setAlignment(Pos.CENTER);
         answerButtonsBox.setEffect(new DropShadow(30, 0, 0, DROP_COLOR));
         // 
@@ -137,11 +133,11 @@ public class DialogPane extends BorderPane {
 //        answerPane.setPrefSize(getPrefWidth()*0.4, getPrefHeight());
 //        answerPane.setEffect(new DropShadow());
 //        answerPane.setBackground(new Background(new BackgroundFill(Color.GOLD, CornerRadii.EMPTY, Insets.EMPTY)));
-        
+
         //HBox hBox = new HBox(leftArea, answerButtonsBox);
         setCenter(leftArea);
         setRight(answerButtonsBox);
-        
+
         // Image of NPC as cameo cropped view
         ImageView npcView = npc.getPoseSheet().getCameo();
         npcView.setY(-npcView.getBoundsInLocal().getHeight());
@@ -153,7 +149,7 @@ public class DialogPane extends BorderPane {
         cameoFrame.setFill(Color.TRANSPARENT);
         AnchorPane.setLeftAnchor(cameoFrame, 0.0);
         AnchorPane.setBottomAnchor(cameoFrame, 0.0);
-        
+
         // Close Dialog control 'X' (upper right of pane.)
         Rectangle closeRect = new Rectangle(40, 40, Color.RED);
         //closeRect.setX(getPrefWidth()-40);
@@ -162,15 +158,17 @@ public class DialogPane extends BorderPane {
         AnchorPane.setBottomAnchor(closeRect, 0.0);
         closeRect.setOnMouseClicked((event) -> {
             event.consume();
-            npc.setTalking(false);
-            setVisible(false);
+            doCloseDialog();
+            //npc.setTalking(false);
+            //setVisible(false);
+
             //setActionDone(false);
         });
-        
+
         AnchorPane topArea = new AnchorPane(cameoFrame, closeRect);
         setTop(topArea);
     }
-    
+
 //    /**
 //     * Initialize the geometry of the dialog screen.  
 //     * Called by Vignette once the screen geometry is known.
@@ -232,7 +230,6 @@ public class DialogPane extends BorderPane {
 //        
 //        //getChildren().addAll(rect, dialogStackPane, closeRect, npcView, cameoFrame);
 //    }
-    
     /**
      * @return the actionDone
      */
@@ -253,10 +250,10 @@ public class DialogPane extends BorderPane {
     public ArrayList<DialogSheet2> getDialogList() {
         return dialogList;
     }
-    
-    public void addDialogSheet( DialogSheet2 ds ) {
+
+    public void addDialogSheet(DialogSheet2 ds) {
         dialogList.add(ds);
-        if ( currentDialogSheet == null ) {
+        if (currentDialogSheet == null) {
             setCurrentDialogSheet(ds);
         }
     }
@@ -277,8 +274,8 @@ public class DialogPane extends BorderPane {
      */
     public void setCurrentDialogSheet(DialogSheet2 ds) {
         dialogText.setText(ds.getDialogText());
-        rebuildResponsePane( ds.getResponse() );
-        
+        rebuildResponsePane(ds.getResponse());
+
         //dialogPane.getChildren().remove(this.currentDialogSheet);
         this.currentDialogSheet = ds;
         //dialogPane.getChildren().add(ds);
@@ -295,33 +292,38 @@ public class DialogPane extends BorderPane {
 //            });
         });
     }
-    
-    private static Button responseButton( DialogResponse2 response ) {
-            Button b = new Button(response.getText());
-            b.setFont(Font.font(FONT_SIZE*0.7));
-            b.setBorder(new Border(
-                    new BorderStroke(Color.RED, 
-                            BorderStrokeStyle.SOLID, 
-                            new CornerRadii(FONT_SIZE/2), 
-                            new BorderWidths(2)
-                    )));
-            b.setOnAction((tt) -> {
-                tt.consume();
-                response.getAction().doResponseAction();                
-            });
-            
-            return b;
+
+    private static Button responseButton(DialogResponse2 response) {
+        Button b = new Button(response.getText());
+        b.setFont(Font.font(FONT_SIZE * 0.7));
+        b.setBorder(new Border(
+                new BorderStroke(Color.RED,
+                        BorderStrokeStyle.SOLID,
+                        new CornerRadii(FONT_SIZE / 2),
+                        new BorderWidths(2)
+                )));
+        b.setOnAction((tt) -> {
+            tt.consume();
+            response.getAction().doResponseAction();
+        });
+
+        return b;
     }
-    
-    public void setExit( VignetteTrigger port) {
+
+    public void setExit(VignetteTrigger port) {
         this.port = port;
     }
-    
+
     /**
      * @return the port
      */
     public VignetteTrigger getExit() {
         return port;
+    }
+
+    public void doCloseDialog() {
+        npc.setTalking(false);
+        setVisible(false);
     }
 
 }
