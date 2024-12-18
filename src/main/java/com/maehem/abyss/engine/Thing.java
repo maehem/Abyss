596f723d30607949b59a632715637d1295e6216b
Mark J Koch <maehem@users.noreply.github.com>
2024-12-18 15:31:11 -0800

    Whitespace formatting.

--- src/main/java/com/maehem/abyss/engine/Thing.java
+++ src/main/java/com/maehem/abyss/engine/Thing.java
@@ -1,28 +1,25 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.abyss.engine;
 
 import static com.maehem.abyss.Engine.LOGGER;
-
-import com.maehem.abyss.Engine;
-import java.io.InputStream;
+import com.maehem.abyss.engine.Character;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-
 import java.util.Properties;
 import java.util.logging.Level;
 import javafx.scene.paint.Color;
@@ -193,7 +190,7 @@ public abstract class Thing {
     public abstract void loadProperties(Properties p);
 
     public abstract String getIconPath();
-    
+
     public int getCondition() {
         return condition;
     }
