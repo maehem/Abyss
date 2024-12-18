d4bba4a06e62dd97b08f211685a935ab88a496db
Mark J Koch <maehem@users.noreply.github.com>
2024-12-18 15:28:37 -0800

    Whitespace reformatting.

--- src/main/java/com/maehem/abyss/Engine.java
+++ src/main/java/com/maehem/abyss/Engine.java
@@ -1,37 +1,37 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
 */
 package com.maehem.abyss;
 
 import com.maehem.abyss.debug.DebugTab;
-import com.maehem.abyss.engine.Loop;
-import com.maehem.abyss.engine.Player;
-import com.maehem.abyss.engine.Vignette;
 import com.maehem.abyss.engine.GameState;
 import com.maehem.abyss.engine.GameStateListener;
+import com.maehem.abyss.engine.Loop;
 import com.maehem.abyss.engine.MatrixPane;
-import com.maehem.abyss.engine.view.ViewPane;
+import com.maehem.abyss.engine.Player;
+import com.maehem.abyss.engine.Vignette;
 import com.maehem.abyss.engine.VignetteTrigger;
-import com.maehem.abyss.engine.view.ChipsConfiguratorPane;
-import com.maehem.abyss.engine.gui.InventoryPane;
+import com.maehem.abyss.engine.bbs.BBSTerminal;
 import com.maehem.abyss.engine.gui.GameControlsPane;
+import com.maehem.abyss.engine.gui.InventoryPane;
 import com.maehem.abyss.engine.gui.NarrationPane;
 import com.maehem.abyss.engine.gui.RomConstructPane;
 import com.maehem.abyss.engine.gui.TerminalPane;
-import com.maehem.abyss.engine.bbs.BBSTerminal;
+import com.maehem.abyss.engine.view.ChipsConfiguratorPane;
+import com.maehem.abyss.engine.view.ViewPane;
 import com.maehem.abyss.logging.LoggingFormatter;
 import com.maehem.abyss.logging.LoggingHandler;
 import com.maehem.abyss.logging.LoggingMessageList;
@@ -72,10 +72,10 @@ public class Engine extends Application implements GameStateListener {
 
     public static final Logger LOGGER = Logger.getLogger("com.maehem.abyss");
     public static final String VERSION = "0.0.0";
-    
+
     //private double SCALE = 0.75;
     private static final double SCALE = 0.66;
-    
+
     private final Group vignetteGroup = new Group();
     private final StackPane topArea = new StackPane();
     private NarrationPane narrationPane;
@@ -93,10 +93,10 @@ public class Engine extends Application implements GameStateListener {
 
     private Stage window;
     private Loop loop;   // Game logic Loop
-    
+
     private final HBox bottomArea = new HBox();  // gui and naration
     private final VBox gamePane = new VBox(new Group(topArea), new Group(bottomArea));
-    
+
     private final StackPane root = new StackPane(gamePane);
     private final Scene scene = new Scene(root); //, 1280, 920);
 
@@ -104,27 +104,27 @@ public class Engine extends Application implements GameStateListener {
     private final LoggingHandler loggingHandler = new LoggingHandler(messageLog);
 
     private final Stage debugWindow = new Stage();
-    
+
     private final GameState gameState;// = new GameState();
-    
-    
+
+
     // TODO:   Music track system
     //          - Blend music scene to scene
-    
+
     // TODO:   Cut Scenes
-    
-    
-    
+
+
+
     public Engine( ) {
         configureLogging();
-        
+
         // TRUE for debug.
         LOGGER.setUseParentHandlers(false);  // Prevent INFO and HIGHER from going to stderr.
         this.gameState = new GameState();
 
     }
-    
-    
+
+
     @Override
     public void start(Stage window) {
         this.window = window;
@@ -132,18 +132,18 @@ public class Engine extends Application implements GameStateListener {
         initDebugWindow();
 
         getGameState().addListenter(this);
-        
+
         LOGGER.info("Abyss Engine version:  " + VERSION);
-        LOGGER.log(Level.INFO, "JavaFX Version: {0}", 
+        LOGGER.log(Level.INFO, "JavaFX Version: {0}",
                 System.getProperties().get("javafx.runtime.version")
         );
         LOGGER.log(Level.INFO, "Game: {0} {1}", new Object[]{gameState.getLongGameName(), gameState.getGameVersion()});
-        
+
         LOGGER.fine("Fine log message.");
         LOGGER.finer("Finer log message.");
         LOGGER.finest("Finest log message.");
-        
-        
+
+
         gameState.init();
 
         splashScreen = new ImageView(new Image(getClass().getResourceAsStream("/splash.png")));
@@ -155,53 +155,53 @@ public class Engine extends Application implements GameStateListener {
             // TODO: Check if user wants to save game.
             Platform.exit();
         });
-        
+
         gameControls = new GameControlsPane(getGameState(), ViewPane.WIDTH/2);
 
         // Narration - Shares space with ROM, but only one is visible at a time.
-        narrationPane = new NarrationPane(ViewPane.WIDTH/2);     
+        narrationPane = new NarrationPane(ViewPane.WIDTH/2);
         // ROM Adviser -- Hint System and Third Hand
         romPane = new RomConstructPane(getGameState(), ViewPane.WIDTH/2);
         StackPane rightPane = new StackPane(narrationPane,romPane);
-        
+
         narrationPane.setVisible(true);
         romPane.setVisible(false);
         bottomArea.getChildren().addAll(gameControls ,rightPane  );
-        
+
         configureGuiLayout();
-        
+
         window.show(); // Show the window so that other GUI elements can layout().
 
         // Inventory
         inventoryPane = new InventoryPane(gameState);
-        
+
         // Chips  -- Confgurable Buffs
         chipsPane = new ChipsConfiguratorPane(gameState);
-        
+
         // Terminal -- Base BBS style system
         terminalPane = new TerminalPane(gameState );
         terminalPane.setTerminal(gameState.getPublicTerminal(), false);
-        
+
         // Deck Bench -- Configure your Deck with inventory components
-        
+
         // Cyberspace  -- ROM Helper replaces Narration Window
         matrixPane = new MatrixPane(gameState );  ///, Vignette.NATIVE_WIDTH, Vignette.NATIVE_HEIGHT);
-        
+
         topArea.getChildren().addAll(vignetteGroup, //splashScreen,
                 chipsPane, inventoryPane, terminalPane, matrixPane
         );
-        
+
         // Initilize the game
         getGameState().load(STARTING_VIGNETTE);
 
         matrixPane.updateHudSoftwareTabs(); /// Might not need this here.
-        
+
         initHotKeys();
         initKeyInput();
-        
+
          // Load the starting room.
         String roomName = getGameState().getProperty(
-                GameState.PROP_CURRENT_VIGNETTE, 
+                GameState.PROP_CURRENT_VIGNETTE,
                 STARTING_VIGNETTE
         );
         notifyVignetteExit(new VignetteTrigger(roomName));  // Just leveraging the Room Loading System here.
@@ -212,7 +212,7 @@ public class Engine extends Application implements GameStateListener {
         DebugTab debugTab = new DebugTab( messageLog, gameState);
         debugTab.setFormatter(loggingHandler.getFormatter());
         Scene debugScene = new Scene(debugTab);
-        
+
         debugWindow.setScene(debugScene);
         debugWindow.setTitle("Debug Window");
         debugWindow.setAlwaysOnTop(true);
@@ -222,7 +222,7 @@ public class Engine extends Application implements GameStateListener {
         debugWindow.setOnShowing((t) -> {
             gameState.setShowDebug(true);
         });
-        
+
         debugWindow.show();
         Rectangle2D bounds = Screen.getPrimary().getBounds();
         debugWindow.setX(bounds.getWidth()-debugWindow.getWidth());
@@ -231,12 +231,12 @@ public class Engine extends Application implements GameStateListener {
 
     private void setVignette(Vignette v) {
         Vignette currentVignette = gameState.getCurrentVignette();
-        
+
         vignetteGroup.getChildren().remove(currentVignette);
         gameState.setCurrentVignette(v);
-        v.loadState(gameState);        
+        v.loadState(gameState);
         vignetteGroup.getChildren().add(0, v);
-        
+
         window.setTitle(v.getName());
 
         //loop = new Loop(this, v);
@@ -263,15 +263,15 @@ public class Engine extends Application implements GameStateListener {
         }
         // Player - stop doing any movement/actions
         gameState.getPlayer().stopAnimating();
-        
-        
+
+
 // Don't do this.  GameState should already have info on vignette changes.
 //        // Save relevant game data/goals from scene?
 //        Vignette currentVignette = gameState.getCurrentVignette();
 //        if ( currentVignette != null ) {
 //            currentVignette.saveState(getGameState());
 //        }
-        
+
         try {
             String packageName = gameState.getContentPack().getClass().getPackageName();
             Class<?> c = Class.forName(packageName + ".content.vignette." + nextRoom.getDestination());
@@ -279,16 +279,16 @@ public class Engine extends Application implements GameStateListener {
             Object object = cons.newInstance( gameState, nextRoom, getPlayer());
             setVignette((Vignette) object);
             LOGGER.log(Level.FINER, "[Engine] Loaded Vignette: {0}", nextRoom.getDestination());
-        } catch (ClassNotFoundException | 
-                NoSuchMethodException | 
-                SecurityException | 
-                InstantiationException | 
-                IllegalAccessException | 
-                IllegalArgumentException | 
+        } catch (ClassNotFoundException |
+                NoSuchMethodException |
+                SecurityException |
+                InstantiationException |
+                IllegalAccessException |
+                IllegalArgumentException |
                 InvocationTargetException   ex) {
             ex.printStackTrace();
         }
-        
+
     }
 
     private void initHotKeys() {
@@ -348,12 +348,12 @@ public class Engine extends Application implements GameStateListener {
     }
 
     public void doExit() {
-        
+
         // TODO:   Ask if user is sure.
-        
+
         // Check if we need to save.
-        
-        
+
+
         LOGGER.warning("Exit called.  Quitting game.");
         window.close();
     }
@@ -365,23 +365,23 @@ public class Engine extends Application implements GameStateListener {
               }
         });
     }
-    
+
     private void configureGuiLayout() {
         topArea.setPrefWidth(ViewPane.WIDTH);
         topArea.setScaleX(SCALE);
         topArea.setScaleY(SCALE);
-        
+
         bottomArea.setPrefWidth(ViewPane.WIDTH);
         bottomArea.setScaleX(SCALE);
         bottomArea.setScaleY(SCALE);
-        
+
         bottomArea.setSpacing(8);
         bottomArea.setPadding(new Insets(6));
         bottomArea.setBackground(new Background(new BackgroundFill(
                 Color.DARKGREY, CornerRadii.EMPTY, Insets.EMPTY
-        )));        
+        )));
     }
-    
+
     private void configureLogging() {
         loggingHandler.setFormatter(new LoggingFormatter());
         // Get the top most logger and add our handler.
@@ -395,9 +395,9 @@ public class Engine extends Application implements GameStateListener {
         // Add console handler as handler of logs
         //Logger.getLogger("com.maehem.abyss").addHandler(handler);
         //Logger.getLogger("com.maehem.abyss").setUseParentHandlers(false);
-        
+
     }
-    
+
     /**
      * @return the inventoryPane
      */
@@ -408,11 +408,11 @@ public class Engine extends Application implements GameStateListener {
     public MatrixPane getMatrixPane() {
         return matrixPane;
     }
-    
+
     public Group getVignetteGroup() {
         return vignetteGroup;
     }
-    
+
     /**
      * @param args the command line arguments
      */
@@ -422,6 +422,7 @@ public class Engine extends Application implements GameStateListener {
 
     @Override
     public void gameStateVignetteChanged(GameState gs) {
+        LOGGER.log(Level.SEVERE, "Engine: Update narration pane.");
         narrationPane.setTitle(gs.getCurrentVignette().getName());
         narrationPane.setText(gs.getCurrentVignette().getNarration());
     }
@@ -443,7 +444,7 @@ public class Engine extends Application implements GameStateListener {
 //        hideSpecialPanes();
 //        chipsPane.setVisible(state);
 //    }
-    
+
 //    private void hideSpecialPanes() {
 //        chipsPane.setVisible(false);
 //        inventoryPane.setVisible(false);
