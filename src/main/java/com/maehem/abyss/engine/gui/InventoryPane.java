48629b1c690d3456b477e7058859be4f3817aa8d
Mark J Koch <maehem@users.noreply.github.com>
2024-12-18 15:32:53 -0800

    Whitespace formatting. Adjust inventory method reference.

--- src/main/java/com/maehem/abyss/engine/gui/InventoryPane.java
+++ src/main/java/com/maehem/abyss/engine/gui/InventoryPane.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
  */
 package com.maehem.abyss.engine.gui;
@@ -97,7 +97,7 @@ public final class InventoryPane extends ViewPane implements GameStateListener {
         slots.setHgap(SLOT_GAP);
 
         updateItemGrid();
-//        for( Thing t: player.getAInventory() ) {
+//        for( Thing t: player.getInventory() ) {
 //            //Button slotButton = Thing.getSlotButton(t);
 //            Button slotButton = InventoryPane.createSlotButton(t);
 //            slots.add(slotButton, col%N_COLS, col/N_COLS );
@@ -105,8 +105,8 @@ public final class InventoryPane extends ViewPane implements GameStateListener {
 //                detailPane.showThing(t);
 //                highlightItem(slotButton);
 //            });
-//            
-//            
+//
+//
 //            col++;
 //        }
         Text text = new Text("Inventory");
@@ -191,10 +191,10 @@ public final class InventoryPane extends ViewPane implements GameStateListener {
     public void updateItemGrid() {
         slots.getChildren().clear();
         int col = 0;
-        for (Thing t : player.getAInventory()) {
+        for (Thing t : player.getInventory()) {
             //Button slotButton = Thing.getSlotButton(t);
             Button slotButton = createSlotButton(t);
-            slots.add(slotButton, col % N_COLS, col / N_COLS);            
+            slots.add(slotButton, col % N_COLS, col / N_COLS);
             slotButton.setOnMouseClicked((event) -> {
                 detailPane.showThing(t);
                 highlightItem(slotButton);
