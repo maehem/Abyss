12df72a1d81ceeba7b5e79c427ce9bcbff949f99
Mark J Koch <maehem@users.noreply.github.com>
2024-12-26 09:52:37 -0800

    Reformat whitespace.

--- src/main/java/com/maehem/abyss/engine/EmptyThing.java
+++ src/main/java/com/maehem/abyss/engine/EmptyThing.java
@@ -1,17 +1,17 @@
 /*
-    Licensed to the Apache Software Foundation (ASF) under one or more 
+    Licensed to the Apache Software Foundation (ASF) under one or more
     contributor license agreements.  See the NOTICE file distributed with this
-    work for additional information regarding copyright ownership.  The ASF 
-    licenses this file to you under the Apache License, Version 2.0 
-    (the "License"); you may not use this file except in compliance with the 
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
     License.  You may obtain a copy of the License at
 
       http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software 
-    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
-    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the 
-    License for the specific language governing permissions and limitations 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
     under the License.
 */
 package com.maehem.abyss.engine;
@@ -20,7 +20,7 @@ import java.util.Properties;
 
 /**
  *
- * @author Mark J Koch <mark at maehem dot com>
+ * @author Mark J Koch [@maehem on GitHub]
  */
 public class EmptyThing extends Thing {
 
@@ -28,9 +28,9 @@ public class EmptyThing extends Thing {
         super("Empty");
     }
 
-    
+
     @Override
-    public Properties saveProperties() { 
+    public Properties saveProperties() {
         return new Properties();
     }
 
@@ -49,7 +49,6 @@ public class EmptyThing extends Thing {
 
     @Override
     public void saveState(String key, Properties p) {
-        return;
     }
 
     @Override
@@ -61,6 +60,6 @@ public class EmptyThing extends Thing {
     public String getDescription() {
         return "Empty Thing";
     }
-    
-    
+
+
 }
