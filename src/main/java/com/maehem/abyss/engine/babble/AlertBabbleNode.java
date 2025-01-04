a695499748f190e28d52ed2467a9202eac45c17f
Mark J Koch <maehem@users.noreply.github.com>
2025-01-04 11:41:05 -0800

    Create AlertBabbleNode.java

---
+++ src/main/java/com/maehem/abyss/engine/babble/AlertBabbleNode.java
@@ -0,0 +1,31 @@
+/*
+    Licensed to the Apache Software Foundation (ASF) under one or more
+    contributor license agreements.  See the NOTICE file distributed with this
+    work for additional information regarding copyright ownership.  The ASF
+    licenses this file to you under the Apache License, Version 2.0
+    (the "License"); you may not use this file except in compliance with the
+    License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+    License for the specific language governing permissions and limitations
+    under the License.
+ */
+package com.maehem.abyss.engine.babble;
+
+/**
+ * Fills dialog with an alert using index text and OK button for going to next
+ * index.
+ *
+ * @author Mark J Koch ( @maehem on GitHub )
+ */
+public class AlertBabbleNode extends BabbleNode {
+
+    public AlertBabbleNode(int... nums) {
+        super(nums);
+    }
+
+}
